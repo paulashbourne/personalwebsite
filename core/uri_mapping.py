4456fa031ba90f5d8eea70495a38f41111ffd855
Paul Ashbourne <paul.ashbourne@hotmail.com>
2014-10-18 19:32:56 -0700

    Initial Commit

---
+++ core/uri_mapping.py
@@ -0,0 +1,11 @@
+from tornado.web import URLSpec, StaticFileHandler
+import handlers as h
+
+URI_MAPPING = [
+    URLSpec(r'/', h.HomePageHandler),
+    URLSpec(r'/about', h.AboutPageHandler),
+    URLSpec(r'/projects', h.ProjectsPageHandler),
+    URLSpec(r'/contact', h.ContactPageHandler),
+    URLSpec(r'/static/(.*)', StaticFileHandler, {'path': 'static/'}),
+    URLSpec(r'.*', h.ErrorPageHandler),
+]
---
+++ handlers.py
@@ -0,0 +1,21 @@
+import tornado.web
+
+class HomePageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/home.html")
+
+class AboutPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/about.html")
+
+class ProjectsPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/projects.html")
+
+class ContactPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/contact.html")
+
+class ErrorPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Error")
