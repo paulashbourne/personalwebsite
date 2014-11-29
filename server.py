abfb2f503ee26b593ec6bc3ce6c6087bbcad1765
Paul Ashbourne <paul.ashbourne@hotmail.com>
2014-11-28 23:38:42 -0800

    Updated home page

--- server.py
+++ server.py
@@ -1,12 +1,9 @@
 import tornado.ioloop
 import tornado.web
-import tornado.options
 from core.uri_mapping import URI_MAPPING
 
 paulashbourne = tornado.web.Application(URI_MAPPING)
 
 if __name__ == "__main__":
-    tornado.options.options['log_file_prefix'].set('/opt/logs/paulashbourne.log')
-    tornado.options.parse_command_line()
-    paulashbourne.listen(80)
+    paulashbourne.listen(8880)
     tornado.ioloop.IOLoop.instance().start()
