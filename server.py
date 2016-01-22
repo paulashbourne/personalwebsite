ca1020ec8d89fd113d92129fcabc9ab52ea2668b
Paul Ashbourne <paul.ashbourne@hotmail.com>
2016-01-22 12:15:54 -0500

    Update content

--- server.py
+++ server.py
@@ -2,8 +2,17 @@ import tornado.ioloop
 import tornado.web
 from core.uri_mapping import URI_MAPPING
 
-paulashbourne = tornado.web.Application(URI_MAPPING)
+settings = {
+    'debug' : True
+}
+
+paulashbourne = tornado.web.Application(
+    URI_MAPPING,
+    **settings
+)
 
 if __name__ == "__main__":
+    print "Server starting..."
     paulashbourne.listen(8888)
+    print "Server listening on port 8888"
     tornado.ioloop.IOLoop.instance().start()
